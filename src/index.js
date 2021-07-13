0852269868a1395e2d015b190b44f06083fcb07b
Gerardo Ramirez <gerardogo145@gmail.com>
2021-07-13 13:08:36 +0200

    :art: Edit links and background

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import "./bootstrap/scss/bootstrap.scss";
 import "./styles/index.scss";
+
+$(document).ready(function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        1000,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
